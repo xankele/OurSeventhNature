259c4d36e9e6215113f37b0713b5f98870b27c2b
maciatko62 <ania.soliwoda62@gmail.com>
2021-11-28 19:09:49 +0100

    Added Menu

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public Transform Player;
+	public float m_speed = 0.1f;
+	Camera mycam;
+
+
+
+	public void Start()
+	{
+		mycam = GetComponent<Camera>();
+	}
+
+	public void Update()
+	{
+
+		mycam.orthographicSize = (Screen.height / 100f) / 0.9f;
+
+		if (Player)
+		{
+
+			transform.position = Vector3.Lerp(transform.position, Player.position, m_speed) + new Vector3(0, 2, -12);
+		}
+	}
+
+}
---
+++ Assets/Scripts/GameOverMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public void TryAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+}
