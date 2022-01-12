b61e74e5bbcbddf13ef8dcacee8caa15e81e488a
maciatko62 <ania.soliwoda62@gmail.com>
2022-01-12 18:54:24 +0100

    File cleanup

---
+++ Assets/Scripts/ChooseCharacter/ChooseCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseCharacter : MonoBehaviour
+{
+    private void Start()
+    {
+        PlayerPrefs.SetInt("Choice", 1);
+    }
+    public void ChooseAndrzej()
+    {
+        PlayerPrefs.SetInt("Choice", 1);
+    }
+    public void ChooseMichal()
+    {
+        PlayerPrefs.SetInt("Choice", 2);
+    }
+}
