eb3e837610f1ecbb8303d819287335182719c916
maciatko62 <ania.soliwoda62@gmail.com>
2022-01-18 20:30:02 +0100

    Added smooth transitions

--- Assets/Scripts/LevelsControlers/NextLevel.cs
+++ Assets/Scripts/LevelsControlers/NextLevel.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
+    public Image img;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            StartCoroutine(HideGame());
+            this.gameObject.GetComponent<Collider2D>().enabled = false;
         }
             
+    }
+    IEnumerator HideGame()
+    {
+        for (int i = 0; i <= 255; i += 10)
+        {
+            img.GetComponent<Image>().color = new Color32(255, 255, 225, (byte)i);
+            yield return new WaitForSeconds(0.04f);
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
     }
 }
