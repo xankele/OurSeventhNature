383a96636675a0c7a37e22083fa5fb2f187f4b73
maciatko62 <ania.soliwoda62@gmail.com>
2022-01-18 19:50:59 +0100

    Intro

--- Assets/ShowIntro.cs
+++ Assets/ShowIntro.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ShowIntro : MonoBehaviour
 {
     public Text text;
+    public Image img;
     void Start()
     {
-        //hej[0] = GetComponent<GameObject>();
-        //hej.GetComponent<Image>().color = new Color32(255, 255, 225, 100);
         StartCoroutine(ShowText());
     }
 
@@ -18,7 +18,23 @@ public class ShowIntro : MonoBehaviour
         for(int i = 0; i <= 255; i += 2)
         {
             text.GetComponent<Text>().color = new Color32(255, 255, 225, (byte)i);
+            img.GetComponent<Image>().color = new Color32(255, 255, 225, (byte)i);
+
             yield return new WaitForSeconds(0.04f);
         }
+    }
+    public void HideThisText()
+    {
+        StartCoroutine(HideText());
+    }
+    IEnumerator HideText()
+    {
+        for (int i = 255; i >= 0; i -= 5)
+        {
+            text.GetComponent<Text>().color = new Color32(255, 255, 225, (byte)i);
+            img.GetComponent<Image>().color = new Color32(255, 255, 225, (byte)i);
+            yield return new WaitForSeconds(0.04f);
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
