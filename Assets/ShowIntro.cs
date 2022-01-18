ba4126da5e23964692f502afb7fad13e803ef0a3
Ania <annamikulak99@gmail.com>
2022-01-18 19:37:51 +0100

    123

---
+++ Assets/ShowIntro.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowIntro : MonoBehaviour
+{
+    public Text text;
+    void Start()
+    {
+        //hej[0] = GetComponent<GameObject>();
+        //hej.GetComponent<Image>().color = new Color32(255, 255, 225, 100);
+        StartCoroutine(ShowText());
+    }
+
+    IEnumerator ShowText()
+    {
+        for(int i = 0; i <= 255; i += 2)
+        {
+            text.GetComponent<Text>().color = new Color32(255, 255, 225, (byte)i);
+            yield return new WaitForSeconds(0.04f);
+        }
+    }
+}
