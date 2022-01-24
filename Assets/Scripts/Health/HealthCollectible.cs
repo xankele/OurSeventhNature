b5c4638cd23b98928b074e0f8bba72f445c02168
maciatko62 <ania.soliwoda62@gmail.com>
2022-01-24 18:32:54 +0100

    Andrzej is walking and jumping

--- Assets/Scripts/Health/HealthCollectible.cs
+++ Assets/Scripts/Health/HealthCollectible.cs
@@ -6,20 +6,34 @@ public class HealthCollectible : MonoBehaviour
 {
     [SerializeField] private float healthValue;
 
+    public AudioSource audio;
+    public AudioClip audioClip;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
             if (GameObject.Find("Player_Andrzej") == true)
             {
+                StartCoroutine(HeartNone());
+                audio.clip = audioClip;
+                audio.Play();
                 collision.GetComponent<HealthA>().AddHealth(healthValue);
-                gameObject.SetActive(false);
+
+
             }
             else
             {
                 collision.GetComponent<HealthM>().AddHealth(healthValue);
                 gameObject.SetActive(false);
+                audio.Play();
             }
         }
+    }
+    IEnumerator HeartNone()
+    {
+        yield return new WaitForSeconds(0.688f);
+        gameObject.SetActive(false);
+
     }
 }
